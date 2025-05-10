0be083ae6175afa13c60f1e1d2bb945cfba462df
Alejandro <alejandropulido2809@gmail.com>
2025-05-11 00:29:27 +0200

    cambio en proyectos que se corregiran en su momento

--- src/pages/Proyectos.jsx
+++ src/pages/Proyectos.jsx
@@ -10,14 +10,14 @@ function Proyectos(){
                     <img src="/img/portafolio.png" alt="Proyecto 1"/>
                     <h3>Social-Match</h3>
                     <p>Descipcioon</p>
-                    <a href="#">Ir a la pagina</a>
+                    {/*<a href="#">Ir a la pagina</a>*/}
                 </div>
             
                 <div className="proyecto-card">
                     <img src="/img/portafolio.png" alt="Proyecto 1"/>
                     <h3>Calculadora Freelancer</h3>
                     <p>Descripcion</p>
-                    <a href="#">Ir a la pagina</a>
+                    {/*<a href="#">Ir a la pagina</a>*/}
                 </div>
                 {/*Se duplicaran más tarjetas para proximos proyectos */}
             </div>
