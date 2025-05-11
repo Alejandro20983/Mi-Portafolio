1a1df0245f250bfbed286ce9b663962993df0e11
Alejandro <alejandropulido2809@gmail.com>
2025-05-11 21:18:12 +0200

    se agrega supabase a contacto

--- src/pages/Contacto.jsx
+++ src/pages/Contacto.jsx
@@ -1,15 +1,41 @@
 import React from "react";
 import './Contacto.css';
+import {supabase} from '../supabaseClient';
+import { useState } from "react";
 
 function Contacto(){
+
+    const [form, setForm] = useState({
+        nombre: '',
+        email: '',
+        mensaje: '',
+    });
+
+    const handleChange = (e) =>{
+        setForm({...form,[e.target.name]: e.target.value});
+    }
+
+    const handleSubmit = async (e) =>{
+        e.preventDefault();
+    
+        const {error} = await supabase.from('contacto').insert([form]);
+
+        if(error){
+            alert('Error al enviar el mensaje')
+            console.error(error);
+        }else{
+            alert('Mensaje enviado con exito')
+            setForm({nombre : '', email:'',mensaje:''});
+        }
+    }
     return(
         <section className="contacto">
             <h2>Contactame</h2>
             <p>Si deseas conversar sobre algún proyecto o un presupuesto para un proyecto contactame</p>
-            <form className="formulario">
-                <input type="text" placeholder="Tu nombre" required/>
-                <input type="email" placeholder="Tu email" required/>
-                <textarea placeholder="Tu mensaje" required></textarea>
+            <form className="formulario" onSubmit={handleSubmit}>
+                <input type="text" placeholder="Tu nombre" value={form.nombre} onChange={handleChange} required/>
+                <input type="email" placeholder="Tu email" value={form.email} onChange={handleChange}  required/>
+                <textarea placeholder="Tu mensaje" value={form.mensaje} onChange={handleChange} required></textarea>
                 <button type="submit">Enviar</button>
             </form>
             <div className="redes-sociales">
